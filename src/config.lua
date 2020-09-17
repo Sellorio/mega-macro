15fed8bbef23f4330512cee42d4d16298ddf1f4b
Sea Millo <sea.millo@outlook.com>
2020-09-17 14:56:23 +1000

    #1 Fixing tab widths and specialization name being None.

--- src/config.lua
+++ src/config.lua
@@ -1,10 +1,9 @@
 function MegaMacro_InitialiseConfig()
-    if MegaMacroConfig == nil then
-        MegaMacroConfig = {
-        }
-
+    if MegaMacroGlobalData == nil then
         MegaMacroGlobalData = {
-            Macros = {}
+            Activated = false,
+            Macros = {},
+            Classes = {}
             -- Macro: { class, spezialization, displayName, code }
         }
     end
@@ -12,8 +11,66 @@ function MegaMacro_InitialiseConfig()
     if MegaMacroCharacterData == nil then
         MegaMacroCharacterData = {
             Enabled = false,
-            Macros = {}
+            Macros = {},
+            Specializations = {}
             -- Macro: { spezialization, displayName, code }
         }
     end
 end
+
+--[[
+MegaMacroGlobalData = {
+    Activated = false,                                          Whether or not the addon is activated in a global scope - copies global macros from native and replaces with mega macros
+    Macros = {
+        [1] = {
+            Id = "DisplayNgg01",                                DisplayName is truncated to 8 characters, padded where needed with spaces and then the scope code is added with the index of the native macro within the scope
+            DisplayName = "DisplayName",
+            Code = "code"
+        }
+    },
+    Classes = {
+        ["Druid"] = {
+            Macros = {
+                [1] = {
+                    Id = "DisplayNgg01",
+                    DisplayName = "DisplayName",
+                    Code = "code"
+                }
+            },
+            Specializations = {
+                ["Restoration"] = {
+                    Macros = {
+                        [1] = {
+                            Id = "DisplayNgg01",
+                            DisplayName = "DisplayName",
+                            Code = "code"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+MegaMacroCharacterData = {
+    Enabled = false,
+    Macros = {
+        [1] = {
+            Id = "DisplayNgg01",
+            DisplayName = "DisplayName",
+            Code = "code"
+        }
+    },
+    Specializations = {
+        ["Restoration"] = {
+            Macros = {
+                [1] = {
+                    Id = "DisplayNgg01",
+                    DisplayName = "DisplayName",
+                    Code = "code"
+                }
+            }
+        }
+    }
+}
+]]
