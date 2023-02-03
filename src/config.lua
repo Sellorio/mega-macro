a400d766624a700016bb635ac193c614c473c4d2
Paul Büchner <paul-buechner@outlook.de>
2023-02-03 02:20:05 +0100

    feat(tooltip): use native GameTooltip for config checkbuttons
    - enables reduced tooltip fontsize

--- src/config.lua
+++ src/config.lua
@@ -39,26 +39,54 @@ end
 -- Create the Config options
 function MecaMacro_GenerateConfig()
     -- Blizzard Macro option
-    MecaMacroConfig_GenerateCheckbox('UseBlizzardMacros', 'Blizzard Style Macros', '250 Char limit. \nUses Blizzards Macro Buttons while keeping MegaMacro interface and organizer. \n\nEnable this if you have stability issues or need to uninstall MegaMacro. \n\nNote: Uninstalling may still cause you to lose class and spec Macros since they are not supported by Blizzard. Move them to global or character tabs before uninstalling.', 0, 0, MegaMacroConfig.UseNativeMacros, function(value)
-        MegaMacroConfig.UseNativeMacros = value
-        MegaMacro_BlizMacro_Toggle()
-    end)
+    MecaMacroConfig_GenerateCheckbox(
+        'UseBlizzardMacros',
+        'Blizzard Style Macros',
+        'Set Character Limit to 250.\n\nUses Blizzards Macro Buttons while keeping\nMegaMacro interface and organizer.\n\nEnable this if you have stability issues or need\nto uninstall MegaMacro.\n\nNote: Uninstalling may still cause you to lose\nclass and spec Macros since they are not\nsupported by Blizzard. Move them to global\nor character tabs before uninstalling.',
+        0,
+        0,
+        MegaMacroConfig.UseNativeMacros,
+        function(value)
+            MegaMacroConfig.UseNativeMacros = value
+            MegaMacro_BlizMacro_Toggle()
+        end
+    )
     -- Blizzard Action Bar Icons
-    MecaMacroConfig_GenerateCheckbox('UseBlizzardActionIcons', 'Blizzard Action Bar Icons', 'Disable Mega Macro Action Bar Engine. \nOnly Use with Blizzard Style Macros', 0, -25, MegaMacroConfig.UseNativeActionBar, function(value)
-        MegaMacroConfig.UseNativeActionBar = value
-    end)
+    MecaMacroConfig_GenerateCheckbox(
+        'UseBlizzardActionIcons',
+        'Blizzard Action Bar Icons',
+        'Disable Mega Macro Action Bar Engine.\n\nOnly Use with Blizzard Style Macros.',
+        0,
+        -25,
+        MegaMacroConfig.UseNativeActionBar,
+        function(value)
+            MegaMacroConfig.UseNativeActionBar = value
+        end
+    )
 end
 
-function MecaMacroConfig_GenerateCheckbox(name, text, tooltip, x, y, checked, onClick)
+function MecaMacroConfig_GenerateCheckbox(name, text, tooltipInfo, x, y, checked, onClick)
     local checkbox = CreateFrame("CheckButton", "MegaMacro_ConfigContainer_" .. name, MegaMacro_ConfigContainer, "ChatConfigCheckButtonTemplate")
     checkbox:SetPoint("TOPLEFT", x, y)
     checkbox:SetChecked(checked)
     checkbox:SetScript("OnClick", function(self)
         onClick(checkbox:GetChecked())
     end)
-    checkbox.tooltip = tooltip
+
+    -- Tooltip
+    checkbox:SetScript("OnEnter", function(self)
+        GameTooltip_SetDefaultAnchor(GameTooltip, self)
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+        GameTooltip:SetText(tooltipInfo, 1, 1, 1)
+        GameTooltip:Show()
+    end)
+
+    checkbox:SetScript("OnLeave", function(self)
+        GameTooltip:Hide()
+    end)
 
     local textFrame = _G[checkbox:GetName() .. "Text"]
+    textFrame:SetPoint("LEFT", checkbox, "RIGHT", 0, 0)
     textFrame:SetFontObject("GameFontNormalSmall")
     textFrame:SetText(text)
 
